ee14f3baf38a9ad05426237a22528c9ca8941040
OleksandrBohatyrov <alexboggg361@gmail.com>
2024-11-07 23:07:23 +0200

    Added User model

---
+++ OnlineStoreAPI/Data/StoreContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStoreAPI.Models;
+using System.Collections.Generic;
+
+namespace OnlineStoreAPI.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
