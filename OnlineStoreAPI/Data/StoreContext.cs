cf5ddf689b3269e0155ee0bf3be586f7b4a83d93
OleksandrBohatyrov <alexboggg361@gmail.com>
2024-11-07 23:28:12 +0200

    added controller for logic

--- OnlineStoreAPI/Data/StoreContext.cs
+++ OnlineStoreAPI/Data/StoreContext.cs
@@ -9,5 +9,8 @@ namespace OnlineStoreAPI.Data
         public StoreContext(DbContextOptions<StoreContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
     }
 }
