e8605dfd2dbf77395c926b8e77321f1dd4590b82
opilane <opilane@ORDI-402566>
2024-11-11 16:12:49 +0200

    фыврдордфыврфы

--- OnlineStoreAPI/Program.cs
+++ OnlineStoreAPI/Program.cs
@@ -3,17 +3,28 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// ????????? CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazorApp",
+        policy => policy.WithOrigins("https://localhost:7151") // ????, ?? ??????? ???????? Blazor-??????????
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
 
-builder.Services.AddControllers();
+// ??????????? ? ???? ??????
 builder.Services.AddDbContext<StoreContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("StoreDatabase"),
     new MySqlServerVersion(new Version(8, 0, 25))));
 
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// ?????????? CORS-????????
+app.UseCors("AllowBlazorApp");
 
 if (app.Environment.IsDevelopment())
 {
@@ -22,9 +33,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
